de42368b32149fe1b1cc108d9bca693c3ab657b6
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2016-11-17 13:01:31 +0100

    crawler baseline added

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 
+from urllib import urlopen
+import re
 
-#Access web site such as Google Play and extract at least 1000 app descriptions
+# Access web site such as Google Play and extract at least 1000 app descriptions
+url = "https://play.google.com/store/apps/top"
+raw = urlopen(url).read()
 
-# Preprocess app descriptions: tokenization, normalization, etc
+# Find links
+links = re.findall("href=\"/store/apps/details.*?\"", raw)
+
+# Transform links to right format
+links = ["https://play.google.com" + link.replace("href=", "").replace("\"", "") + "&hl=en" for link in links]
+
+appRaw = urlopen(links[0]).read().decode('utf8')
+description = re.findall("itemprop=\"description.*?\">.*?<div jsname=\".*?\">.*?</div>", appRaw)
+print description
+
+# Pre-process app descriptions: tokenization, normalization, etc
 
 # Compute and store tf, df in the inverted index
 
