c276b3a59d7ded6445b5eea5af54917e23451d98
petraohlin8 <petraohlin8@gmail.com>
2016-11-17 11:32:26 +0100

    Added pseudo code

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+#Access web site such as Google Play and extract at least 1000 app descriptions
+
+# Preprocess app descriptions: tokenization, normalization, etc
+
+# Compute and store tf, df in the inverted index
+
+# Write a ranked query processor using vector space model
+
