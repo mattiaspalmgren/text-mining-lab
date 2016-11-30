29dcab194bf86799223e09f8709b063388246266
petraohlin8 <petraohlin8@gmail.com>
2016-11-30 13:27:38 +0100

    split into test and training sets and do the training

--- text_classification/main.py
+++ text_classification/main.py
@@ -1,8 +1,9 @@
 from nltk.corpus import movie_reviews
-import nltk.probability
-import random
+import nltk.probability, nltk.metrics
+import random, math
 
 nltk.download("movie_reviews")
+
 
 # Build data structure for the movie reviews
 documents = [(list(movie_reviews.words(fileid)), category)
@@ -21,5 +22,15 @@ def document_feature(document):
     for word in word_features:
         features['contains({})'.format(word)] = (word in document_word)
     return features
+
+N = len(documents)
+feature_sets = [(document_feature(d), c) for (d, c) in documents]
+train_set, test_set = feature_sets[math.floor(N*0.8):], feature_sets[:math.floor(N*0.8)]
+
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+test_set_without_lables = [d for (d, l) in test_set]
+test_labels = [l for (d, l) in test_set]
+predictions = classifier.classify_many(test_set_without_lables)
 
 
